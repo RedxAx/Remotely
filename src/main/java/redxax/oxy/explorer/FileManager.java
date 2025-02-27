c8739f70868713a97baaeccd25c1625f447ff8a6
RedxAx <144875043+RedxAx@users.noreply.github.com>
2025-02-27 03:40:08 +0300

    Fixed Broken Deletion For Remote Folders That Contains Spaces.

--- src/main/java/redxax/oxy/explorer/FileManager.java
+++ src/main/java/redxax/oxy/explorer/FileManager.java
@@ -64,7 +64,7 @@ public class FileManager {
                 try {
                     String remotePath = path.toString().replace("\\", "/");
                     String fileName = Paths.get(remotePath).getFileName().toString();
-                    String trashPath = homeDir + "/remotely/data/trash/\"" + fileName + "\"";
+                    String trashPath = homeDir + "/remotely/data/trash/" + fileName;
 
                     if (sshManager.remoteFileExists(homeDir + "/remotely/data/trash")) {
                         sshManager.runRemoteCommand("mkdir -p " + homeDir + "/remotely/data/trash " + "mv -f \"" + remotePath + "\" \"" + trashPath + "\"");
