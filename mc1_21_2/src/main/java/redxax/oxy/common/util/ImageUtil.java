832e702e952ff219518c226544f368509ae3f66e
RedxAx <144875043+RedxAx@users.noreply.github.com>
2025-03-16 14:10:31 +0300

    More Clean Up.

--- mc1_21_2/src/main/java/redxax/oxy/common/util/ImageUtil.java
+++ mc1_21_2/src/main/java/redxax/oxy/common/util/ImageUtil.java
@@ -51,7 +51,8 @@ public class ImageUtil {
                 }
             }
             NativeImageBackedTexture texture = new NativeImageBackedTexture(nativeImage);
-            textureId = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("image_" + image.hashCode(), texture);
+            textureId = Identifier.tryParse("redxax.oxy:image_" + image.hashCode());
+            MinecraftClient.getInstance().getTextureManager().registerTexture(textureId, texture);
             textureCache.put(image, textureId);
         }
         context.drawTexture(RenderLayer::getGuiTextured, textureId, x, y, 0F, 0F, width, height, width, height);
@@ -72,7 +73,8 @@ public class ImageUtil {
                 }
             }
             NativeImageBackedTexture texture = new NativeImageBackedTexture(nativeImage);
-            textureId = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("image_" + image.hashCode(), texture);
+            textureId = Identifier.tryParse("redxax.oxy:image_" + image.hashCode());
+            MinecraftClient.getInstance().getTextureManager().registerTexture(textureId, texture);
             textureCache.put(image, textureId);
         }
         context.drawTexture(RenderLayer::getGuiTextured, textureId, x, y, 0F, 0F, width, height, width, height);
