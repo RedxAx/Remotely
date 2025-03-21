979afa6e4a74f67cd1d616c23b713ec01c19cdf9
RedxAx <144875043+RedxAx@users.noreply.github.com>
2025-03-21 12:31:25 +0300

    Followup of The Previous Commit For All Versions.

--- mc1_21_0/src/main/java/redxax/oxy/common/util/ImageUtil.java
+++ mc1_21_0/src/main/java/redxax/oxy/common/util/ImageUtil.java
@@ -14,9 +14,32 @@ import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import static redxax.oxy.common.util.DevUtil.devPrint;
 
 public class ImageUtil {
     static final Map<BufferedImage, Identifier> textureCache = new ConcurrentHashMap<>();
+
+    public static class IconWithTooltip {
+        private BufferedImage image;
+        private final String tooltip;
+
+        public IconWithTooltip(String imagePath, String tooltip) {
+            try {
+                this.image = loadResourceIcon(imagePath);
+            } catch (Exception e) {
+                devPrint("Failed to load icon: " + e.getMessage());
+            }
+            this.tooltip = tooltip;
+        }
+
+        public BufferedImage getImage() {
+            return image;
+        }
+
+        public String getTooltip() {
+            return tooltip;
+        }
+    }
 
     public static BufferedImage loadResourceIcon(String path) throws Exception {
         try (InputStream is = redxax.oxy.common.util.ImageUtil.class.getResourceAsStream(path)) {
