b832e5f75179b43ff7d5137afeb18394b6e08c57
RedxAx <144875043+RedxAx@users.noreply.github.com>
2025-04-19 00:12:34 +0300

    Mixinininin.

---
+++ mc1_21_0/src/main/java/redxax/oxy/common/mixin/accessor/SliderWidgetAccessor.java
@@ -0,0 +1,11 @@
+package redxax.oxy.common.mixin.accessor;
+
+import net.minecraft.client.gui.widget.SliderWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(SliderWidget.class)
+public interface SliderWidgetAccessor {
+    @Accessor("value")
+    double getValue();
+}
