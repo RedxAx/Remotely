ac5d34e63581933f414bd23f8e83396da16a62ba
RedxAx <144875043+RedxAx@users.noreply.github.com>
2025-04-19 23:30:06 +0300

    Final Mixin Changes.

--- common/src/main/java/redxax/oxy/common/mixin/accessor/SliderWidgetAccessor.java
+++ common/src/main/java/redxax/oxy/common/mixin/accessor/SliderWidgetAccessor.java
@@ -1,8 +1,8 @@
 package redxax.oxy.common.mixin.accessor;
 
+import net.minecraft.client.gui.widget.SliderWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import net.minecraft.client.gui.widget.SliderWidget;
 
 @Mixin(SliderWidget.class)
 public interface SliderWidgetAccessor {
