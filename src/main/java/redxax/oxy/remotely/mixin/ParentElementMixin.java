f7887659bb58314e29eca993ac0856986032d8aa
RedxAx <144875043+RedxAx@users.noreply.github.com>
2025-04-30 22:37:16 +0300

    Rewrote Server Factory, And Supported Leaf. But It Will Be Rewrote Again.

--- src/main/java/redxax/oxy/remotely/mixin/ParentElementMixin.java
+++ src/main/java/redxax/oxy/remotely/mixin/ParentElementMixin.java
@@ -1,19 +1,19 @@
-package redxax.oxy.remotely.mixin;//package redxax.oxy.remotely.mixin;
+//package redxax.oxy.remotely.mixin;//package redxax.oxy.remotely.mixin;
 //
+//import net.minecraft.client.gui.ParentElement;
 //import org.spongepowered.asm.mixin.Mixin;
 //import org.spongepowered.asm.mixin.injection.At;
 //import org.spongepowered.asm.mixin.injection.Inject;
 //import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 //import static redxax.oxy.remotely.Render.scaleScroll;
 //
-//import net.minecraft.client.gui.components.events.ContainerEventHandler;
 //
-//@Mixin(value = ContainerEventHandler.class, targets = "net.minecraft.client.gui.ParentElement")
+//@Mixin(value = ParentElement.class)
 //public interface ParentElementMixin { //disabled for now
 //
 //
 //    @Inject(method = "mouseScrolled", at = @At("HEAD"))
-//    default void mouseScrolled(double mouseX, double mouseY, double vertAmount, CallbackInfoReturnable<Boolean> cir) {
-//        scaleScroll(vertAmount);
+//    default void mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount, CallbackInfoReturnable<Boolean> cir) {
+//        scaleScroll(verticalAmount);
 //    }
 //}
