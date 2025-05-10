d1a4f3eb37a2a2e28abc00329d64eaca94d3b4ab
RedxAx <RedxAx10@gmail.com>
2025-05-10 21:59:46 +0300

    Feat-NTF: New Interactable Notifications.

--- src/main/java/redxax/oxy/remotely/mixin/ParentElementMixin.java
+++ src/main/java/redxax/oxy/remotely/mixin/ParentElementMixin.java
@@ -1,19 +1,24 @@
-//package redxax.oxy.remotely.mixin;//package redxax.oxy.remotely.mixin;
-//
-//import net.minecraft.client.gui.ParentElement;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-//import static redxax.oxy.remotely.Render.scaleScroll;
-//
-//
-//@Mixin(value = ParentElement.class)
-//public interface ParentElementMixin { //disabled for now
-//
-//
-//    @Inject(method = "mouseScrolled", at = @At("HEAD"))
-//    default void mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount, CallbackInfoReturnable<Boolean> cir) {
-//        scaleScroll(verticalAmount);
-//    }
-//}
+package redxax.oxy.remotely.mixin;
+
+import net.minecraft.client.gui.ParentElement;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import redxax.oxy.remotely.util.Notification;
+
+
+@Mixin(value = ParentElement.class)
+public interface ParentElementMixin {
+
+
+    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
+    private void mouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        for (Notification notification : Notification.getActiveNotifications()) {
+            if (notification.mouseClicked(mouseX, mouseY, button)) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+
+}
