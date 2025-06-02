14b96275467c1a9fd625645909fafabd6a1db0ef
RedxAx <RedxAx@gmail.com>
2025-06-02 05:15:11 +0300

    Big Improvements In ReScreen: Container Saving, Tab Linking.

--- src/main/java/redxax/oxy/remotely/platforms/neoforge/RemotelyNeoForge.java
+++ src/main/java/redxax/oxy/remotely/platforms/neoforge/RemotelyNeoForge.java
@@ -1,5 +1,5 @@
 //? if neoforge {
-package redxax.oxy.remotely.platforms.neoforge;
+/*package redxax.oxy.remotely.platforms.neoforge;
 
 import net.minecraft.client.MinecraftClient;
 import redxax.oxy.remotely.RemotelyInit;
@@ -8,8 +8,8 @@ import net.neoforged.fml.ModList;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 //? if <1.21 {
-/*import net.neoforged.neoforge.client.ConfigScreenHandler;
-*///?} else {
+/^import net.neoforged.neoforge.client.ConfigScreenHandler;
+^///?} else {
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import redxax.oxy.remotely.servers.SettingsScreen;
 
@@ -22,11 +22,11 @@ public class RemotelyNeoForge {
         RemotelyInit.entrypoint(new NeoForgePlatform());
         ModLoadingContext.get().registerExtensionPoint(
                 //? if <1.21 {
-                /*ConfigScreenHandler.ConfigScreenFactory.class,
+                /^ConfigScreenHandler.ConfigScreenFactory.class,
                 () -> new ConfigScreenHandler.ConfigScreenFactory(
                         ((client, parent) -> new SettingsScreen("config", parent, remotelyDir.toString(), SettingsScreen.settings)
                 )
-                *///?} else {
+                ^///?} else {
                 IConfigScreenFactory.class, () -> (client, parent) -> new SettingsScreen("config", parent, remotelyDir.toString(), SettingsScreen.settings)
                 //?}
         );
@@ -43,4 +43,4 @@ public class RemotelyNeoForge {
         }
     }
 }
-//?}
+*///?}
