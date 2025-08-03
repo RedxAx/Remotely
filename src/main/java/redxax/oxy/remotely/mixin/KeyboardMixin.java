cc41ab2e6fed7d05aefe898d3cb982ec557e2f14
RedxAx <RedxAx10@gmail.com>
2025-08-03 06:46:52 +0300

    Made Narrator Shortcut Canceled In Terminals.

---
+++ src/main/java/redxax/oxy/remotely/mixin/KeyboardMixin.java
@@ -0,0 +1,22 @@
+package redxax.oxy.remotely.mixin;
+
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import redxax.oxy.remotely.terminal.MultiTerminalScreen;
+
+@Mixin(value = Keyboard.class)
+public class KeyboardMixin {
+
+    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
+    private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        if (MinecraftClient.getInstance().currentScreen instanceof MultiTerminalScreen && Screen.hasControlDown() && key == GLFW.GLFW_KEY_B) {
+            ci.cancel();
+        }
+    }
+}
