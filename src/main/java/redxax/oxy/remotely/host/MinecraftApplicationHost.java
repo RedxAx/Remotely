4711817cf37e1816aab4403fa2a96c6b95c93098
RedxAx <RedxAx10@gmail.com>
2025-11-30 19:27:38 +0300

    Support For 1.20.[6, 4, 2, 1]

--- src/main/java/redxax/oxy/remotely/host/MinecraftApplicationHost.java
+++ src/main/java/redxax/oxy/remotely/host/MinecraftApplicationHost.java
@@ -40,7 +40,11 @@ public class MinecraftApplicationHost implements ApplicationHost {
 
     @Override
     public Object getFontIdentifier(String namespace, String path) {
-        return ResourceLocation.fromNamespaceAndPath(namespace, path);
+        //#if MC >= 1.21.1
+        //$$ return ResourceLocation.fromNamespaceAndPath(namespace, path);
+        //#else
+        return new ResourceLocation(namespace, path);
+        //#endif
     }
 
     @Override
