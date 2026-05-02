93043f2efb04240883d3d49dfedb2e984f9d1bae
RedxAx <RedxAx10@gmail.com>
2026-05-02 04:07:13 +0300

    Adapt: Rebase SSH Server Management Overhaul.

---
+++ RemotelyAgent/lifecycle_stub.go
@@ -0,0 +1,37 @@
+//go:build !linux && !darwin && !freebsd
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func lifecycleUnsupported(name string) {
+	fmt.Fprintf(os.Stderr, "%s is not supported on this platform\n", name)
+	os.Exit(1)
+}
+
+func cmdLifecycleStart(args []string) {
+	lifecycleUnsupported("lifecycle-start")
+}
+
+func cmdLifecycleStop(args []string) {
+	lifecycleUnsupported("lifecycle-stop")
+}
+
+func cmdLifecycleSend(args []string) {
+	lifecycleUnsupported("lifecycle-send")
+}
+
+func cmdLifecycleStatus(args []string) {
+	lifecycleUnsupported("lifecycle-status")
+}
+
+func cmdLifecycleConsole(args []string) {
+	lifecycleUnsupported("lifecycle-console")
+}
+
+func cmdLifecycleSupervise(args []string) {
+	lifecycleUnsupported("lifecycle-supervise")
+}
