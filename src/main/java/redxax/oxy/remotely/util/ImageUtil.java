2a7d8498673270f4b872f1c105fa97eb0a8f5ddf
RedxAx <RedxAx10@gmail.com>
2026-05-10 21:54:29 +0300

    Feat(Huge): Background Local Servers, Auto-Restart.

--- src/main/java/redxax/oxy/remotely/util/ImageUtil.java
+++ src/main/java/redxax/oxy/remotely/util/ImageUtil.java
@@ -6,15 +6,13 @@ import java.io.InputStream;
 
 public class ImageUtil {
 
-
     public static BufferedImage loadResourceIcon(String path) {
         return restudio.rescreen.util.ImageUtils.loadIcon(path);
     }
 
     public static BufferedImage loadSpriteSheet(String path) throws Exception {
         try (InputStream is = ImageUtil.class.getResourceAsStream(path)) {
-            if (is == null)
-                throw new Exception("Resource not found: " + path);
+            if (is == null) throw new Exception("Resource not found: " + path);
             return ImageIO.read(is);
         }
     }
