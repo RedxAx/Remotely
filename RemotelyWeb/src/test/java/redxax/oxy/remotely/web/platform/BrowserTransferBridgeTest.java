a2054940a8d10e78f8d4326281c4781bda0de5cd
RedxAx <RedxAx10@gmail.com>
2026-09-13 07:59:01 +0300

    Fix: Release Cancelled Browser File Reads.

--- RemotelyWeb/src/test/java/redxax/oxy/remotely/web/platform/BrowserTransferBridgeTest.java
+++ RemotelyWeb/src/test/java/redxax/oxy/remotely/web/platform/BrowserTransferBridgeTest.java
@@ -2,10 +2,12 @@ package redxax.oxy.remotely.web.platform;
 
 import org.junit.jupiter.api.Test;
 import restudio.rebase.backend.TransferSource;
+import restudio.rescreen.platform.Async;
 import restudio.rescreen.platform.browser.BrowserFile;
 import restudio.rescreen.platform.browser.BrowserHostActionHandler;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,8 +26,32 @@ class BrowserTransferBridgeTest {
         assertEquals(1, host.releases);
     }
 
+    @Test
+    void cancellingAFileReadReleasesTheSelection() {
+        TrackingHost host = new TrackingHost();
+        TransferSource source = BrowserTransferBridge.source(host, new BrowserFile("image.png", "image/png", 4, "", 9, 0));
+
+        Async<TransferSource.Chunk> read = source.next();
+        read.cancel();
+
+        assertEquals(1, host.cancelledReads);
+        assertEquals(1, host.releases);
+    }
+
     private static final class TrackingHost extends BrowserHostActionHandler {
         private int releases;
+        private int cancelledReads;
+
+        @Override
+        public int readFileChunk(BrowserFile file, long offset, int length, Consumer<BrowserFile.Chunk> callback) {
+            return 12;
+        }
+
+        @Override
+        public void cancelFileChunk(int readId) {
+            assertEquals(12, readId);
+            cancelledReads++;
+        }
 
         @Override
         public void releaseFileSelection(int selectionId) {
